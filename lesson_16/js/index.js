97d3baac2664533bc39c60a09522be0b44646fd1
TinaKovalova <itskills4practice@gmail.com>
2024-05-14 23:10:12 +0300

    HW16

--- lesson_16/js/index.js
+++ lesson_16/js/index.js
@@ -1,8 +1,16 @@
 const languagesListBlock = document.querySelector(".laguages__list-block");
-const langugesBtns = document.querySelectorAll(".laguages-btn");
+const langugesBtns = document.querySelectorAll(".laguages__btn");
+const activeContactsBtn = document.querySelector(".contacts__btn");
+const contactsBlock = document.querySelector(".body-header__contacts");
 
+console.log(langugesBtns)
 
-for(let element of langugesBtns )
-element.addEventListener("click", () => {
-    languagesListBlock.classList.toggle("laguages__list-block--hidden");
+for (let element of langugesBtns) {
+    element.addEventListener("click", () => {
+        languagesListBlock.classList.toggle("laguages__list-block--hidden");
+    });
+}
+
+activeContactsBtn.addEventListener("click", () => {
+    contactsBlock.classList.toggle("body-header__contacts--opened")
 })
