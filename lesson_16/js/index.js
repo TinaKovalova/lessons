3d6513231a96ebc5b68fcd3dc40731796c829b58
TinaKovalova <itskills4practice@gmail.com>
2024-05-15 21:18:03 +0300

    HW16

--- lesson_16/js/index.js
+++ lesson_16/js/index.js
@@ -5,6 +5,8 @@ const contactsMainBtn = document.querySelector(".contacts__main-btn");
 const contactsBlock = document.querySelector(".body-header__contacts");
 const burgerMenuBtn = document.querySelector(".burger-menu-btn");
 const burgerHeader = document.querySelector(".burger-header");
+const formActiveBtn = document.querySelector(" .header__active-form-btn");
+const searchForm = document.querySelector(".body-header__form");
 
 console.log(burgerMenuBtn)
 
@@ -20,5 +22,9 @@ activeContactsBtn.addEventListener("click", () => {
     contactsBlock.classList.toggle("body-header__contacts--opened")
 })
 burgerMenuBtn.addEventListener("click", () => {
-    burgerHeader.classList.toggle("header__burger--open");
+    burgerHeader.classList.toggle("header__burger--opened");
+})
+
+formActiveBtn.addEventListener("click", () => {
+    searchForm.classList.toggle("body-header__form--active");
 })
