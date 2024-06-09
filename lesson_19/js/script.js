4441810d33ddc27c406d665a364d43bc3c6a3edf
TinaKovalova <kovalevatkv@gmail.com>
2024-06-09 22:40:40 +0300

    HW19

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const bottomHeader = document.querySelector('.header__bottom');
 
-burgerBtn.addEventListener("click", () => {
+burgerBtn.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("burger-btn--open");
     bottomHeader.classList.toggle("header__bottom--open");
 })
