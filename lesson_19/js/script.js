9ba4d40a63d15ac599beafaee257da677153c41d
TinaKovalova <kovalevatkv@gmail.com>
2024-06-09 20:10:47 +0300

    HW19 burger menu

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -1,9 +1,6 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const bottomHeader = document.querySelector('.header__bottom');
 
-console.log(burgerBtn);
-console.log("help1")
-
 burgerBtn.addEventListener("click", () => {
     bottomHeader.classList.toggle("header__bottom--open");
 })
